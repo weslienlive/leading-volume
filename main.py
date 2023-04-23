f91aa9d55dbb1464b3ce35d2330d5be1d0b8ff45
weslienlive <weslienlive@gmail.com>
2023-04-23 07:39:25 +0200

    refactor to use only bs4

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
+import requests
 from bs4 import BeautifulSoup
 from time import sleep
-import requests
 from dotenv import load_dotenv
 import os
 
@@ -16,120 +11,64 @@ def configure():
 configure()
 
 
-options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('--log-level=3')
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")
-driver_manager = ChromeDriverManager()
-driver = webdriver.Chrome(service=Service(executable_path=driver_manager.install()), options=options)
+TOKEN_BOT = os.getenv('TOKEN_BOT')
+CHAT_ID = os.getenv('CHAT_ID')
 url = 'https://www.coingecko.com/en/watchlists/high-volume'
-driver.get(url)
+exchanges = ['Upbit', 'Bithumb', 'Paribu', 'BtcTurk PRO']
+prev_data = {}
+
+def send_telegram_message(message):
+    url = f'https://api.telegram.org/bot{TOKEN_BOT}/sendMessage'
+    params = {'chat_id': CHAT_ID, 'text': message}
+    response = requests.post(url, params=params)
+    if response.status_code != 200:
+        print(f'Failed to send message to Telegram. Error code: {response.status_code}')
 
 
+while True:
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-def main():
+    tbody = soup.find('tbody', {'data-target': 'currencies.contentBox'})
 
-    # initialize the previous lead_cex as empty
-    previous_lead_cex = {}
+    high_volume_coins = []
 
-    while True:
-        try:
-            # Check if the table element is present in the page's DOM
-            table = driver.find_elements(By.XPATH, "//tbody[@data-target='currencies.contentBox']")
+    for tr in tbody.find_all('tr'):
+        td = tr.find('td', {'data-sort': True})
+        if td:
+            url = td.find('a')['href']  # get the URL from the anchor tag
+            text = td.text.strip()
+            coin_name, symbol = text.split('\n\n')
+            coin = {}
+            coin['coin_name'] = coin_name
+            coin['url'] = url  # add the URL to the dictionary
 
-            # get all the divs with class tw-flex.center
-            divs = table[0].find_elements(By.CLASS_NAME, 'tw-flex.center')
-            print('Table found.')
+            markets_url = f"https://www.coingecko.com{coin['url']}"
+            response_markets = requests.get(markets_url)
+            soup_markets = BeautifulSoup(response_markets.content, 'html.parser')
+            tbody_markets = soup_markets.find('tbody', {'data-target': 'gecko-table.paginatedShowMoreTbody'})
+            tr_markets = tbody_markets.find('tr')
+            tds = tr_markets.find_all('td')
+            exchange = tds[1].find('a').text.strip()
 
-            links = {}
-            lead_cex = {}
+            pair = tds[2].find('a').text.strip()
+            volume = tds[8].text.strip()
+            coin['exchange'] = exchange
+            coin['pair'] = pair
+            coin['volume'] = volume
+            key = f"{exchange}:{pair}:{volume}"
 
-            # loop through all the divs in divs list
-            for div in divs:
-                # get the link from the div
-                link = BeautifulSoup(div.get_attribute('innerHTML'), 'html.parser').find('a')
-                href = link['href']
-                coin_name = link.text.split('\n')[2]
-
-                # skip storing the href if the coin is in the given list
-                if coin_name in ['Tether', 'Bitcoin', 'Ethereum', 'USD Coin', 'Binance USD', 'WETH', 'Dai', 'Wrapped BNB', 'Wrapped AVAX']:
-                    continue
+            if key not in prev_data and exchange in exchanges:
+                high_volume_coins.append(coin)
+                prev_data['markets'] = key
+                print(key)
                 
-                links[coin_name] = href
+                message = f'''
+                EXCHANGE: {exchange}
+                NAME: {coin_name}
+                SYMBOL: {pair}
+                VOLUME: {volume}
+                '''
+                send_telegram_message(message)
 
-            # loop through all the links in the links dictionary
-            for coin_name, href in links.items():
-                # navigate to the coin page
-                driver.get(f'https://www.coingecko.com{href}')
-                sleep(2)  # wait for the page to load
-                
-                # extract the required information from the page source
-                soup = BeautifulSoup(driver.page_source, 'html.parser')
-                tbody = soup.find('tbody', {'data-target': 'gecko-table.paginatedShowMoreTbody'})
-                Exchange = tbody.find('a').text
-                Symbol = tbody.find_all('td')[2].find('a').text.strip()
-                Volume = tbody.find_all('td')[8].text.strip()
-                
-                # store the extracted information if the exchange is one of the leading exchanges
-                if Exchange in ['Upbit', 'Bithumb', 'Paribu', 'BtcTurk PRO']:
-                    lead_cex[coin_name] = {'Exchange': Exchange, 'Symbol': Symbol, 'Volume' : Volume}
-
-                
-                # navigate back to the watchlist page
-                driver.back()
-                sleep(2)  # wait for the page to load
-
-
-            # check if the new information stored in lead_cex is different from the previous_lead_cex
-            if lead_cex != previous_lead_cex:
-
-                # if the information is different, loop through each coin in lead_cex and update only the data points that are different
-                for coin_name, data in lead_cex.items():
-
-                    # use setdefault() to get the previous data or an empty dictionary if the coin is new
-                    previous_data = previous_lead_cex.setdefault(coin_name, {})
-
-                    # update the previous data with only the data points that are different
-                    for key, value in data.items():
-
-                        if previous_data.get(key) != value:
-                            previous_data[key] = value
-
-                            # send a telegram message with the updated data
-                            telegram_message({coin_name: previous_data})
-
-                # update previous_lead_cex with the new information
-                previous_lead_cex = lead_cex
-
-
-        except Exception as e:
-            print('An error occurred:', e)
-
-        finally:
-            # close the driver
-            driver.quit()
-
-        sleep(300)
-
-
-def telegram_message(lead_cex):
-    # send a telegram message
-    chat_id = os.getenv('CHAT_ID')
-    bot_token = os.getenv('BOT_TOKEN')
-
-    for coin_name, data in lead_cex.items():
-        exchange = data["Exchange"]
-        symbol = data["Symbol"]
-        volume = data["Volume"]
-        message = f"Coin: {coin_name}\nExchange: {exchange}\nSymbol: {symbol}\nVolume: {volume}"
-        send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}'
-        response = requests.get(send_text)
-        print(response.content)
-
-
-
-
-main()
+    sleep(900)
