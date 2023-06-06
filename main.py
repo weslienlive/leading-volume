2a73675be5e37f273699b429f65940340640ed50
weslienlive <weslienlive@gmail.com>
2023-06-06 15:59:47 +0200

    add error handling

--- main.py
+++ main.py
@@ -28,43 +28,47 @@ def send_telegram_message(message):
         print(f'Failed to send message to Telegram. Error code: {response.status_code}')
 
 
-while True:
-    response = requests.get(coingecko_url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+try:
+    while True:
+        response = requests.get(coingecko_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
 
-    tbody = soup.find('tbody', {'data-target': 'currencies.contentBox'})
+        tbody = soup.find('tbody', {'data-target': 'currencies.contentBox'})
 
-    high_volume_coins = []
+        high_volume_coins = []
 
-    for tr in tbody.find_all('tr'):
-        td = tr.find('td', {'data-sort': True})
-        if td:
-            url = td.find('a')['href']  # get the URL from the anchor tag
-            text = td.text.strip()
-            coin_name, symbol = text.split('\n\n')
-            coin = {}
-            coin['coin_name'] = coin_name
-            coin['url'] = url  # add the URL to the dictionary
+        for tr in tbody.find_all('tr'):
+            td = tr.find('td', {'data-sort': True})
+            if td:
+                url = td.find('a')['href']  # get the URL from the anchor tag
+                text = td.text.strip()
+                coin_name, symbol = text.split('\n\n')
+                coin = {}
+                coin['coin_name'] = coin_name
+                coin['url'] = url  # add the URL to the dictionary
 
-            markets_url = f"https://www.coingecko.com{coin['url']}"
-            response_markets = requests.get(markets_url)
-            soup_markets = BeautifulSoup(response_markets.content, 'html.parser')
-            tbody_markets = soup_markets.find('tbody', {'data-target': 'gecko-table.paginatedShowMoreTbody'})
-            tr_markets = tbody_markets.find('tr')
-            tds = tr_markets.find_all('td')
-            exchange = tds[1].find('a').text.strip()
+                markets_url = f"https://www.coingecko.com{coin['url']}"
+                response_markets = requests.get(markets_url)
+                soup_markets = BeautifulSoup(response_markets.content, 'html.parser')
+                tbody_markets = soup_markets.find('tbody', {'data-target': 'gecko-table.paginatedShowMoreTbody'})
+                tr_markets = tbody_markets.find('tr')
+                tds = tr_markets.find_all('td')
+                exchange = tds[1].find('a').text.strip()
 
-            pair = tds[2].find('a').text.strip()
-            volume = tds[8].text.strip()
-            coin['exchange'] = exchange
-            coin['pair'] = pair
-            coin['volume'] = volume
-            key = f"{exchange}:{pair}:{volume}"
+                pair = tds[2].find('a').text.strip()
+                volume = tds[8].text.strip()
+                coin['exchange'] = exchange
+                coin['pair'] = pair
+                coin['volume'] = volume
+                key = f"{exchange}:{pair}:{volume}"
 
-            if key not in prev_data and exchange in exchanges:
-                high_volume_coins.append(coin)
-                prev_data['markets'] = key
-                print(key)
-                send_telegram_message(key)
+                if key not in prev_data and exchange in exchanges:
+                    high_volume_coins.append(coin)
+                    prev_data['markets'] = key
+                    print(key)
+                    send_telegram_message(key)
 
-    sleep(4*60*60)
+        sleep(4*60*60)
+
+except Exception as e:
+    send_telegram_message(e)
